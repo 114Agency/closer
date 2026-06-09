3a03d963de9ff593e27b7bd20b80348ebf6910af
Hamza <hamzabouazza55@gmail.com>
2026-06-09 12:31:31 +0000

    modification de url

--- setup_webhook.py
+++ setup_webhook.py
@@ -4,7 +4,9 @@ import requests
 api_key = "170S58NXD1HDE0XHYIWG6GSGWJG3RQV5" 
 
 # 2. Remplace par ton adresse ngrok exacte (sans oublier /whatsapp/incoming à la fin !)
-ngrok_url = "https://outfield-pronounce-handheld.ngrok-free.dev/whatsapp/incoming" 
+# ngrok_url = "https://outfield-pronounce-handheld.ngrok-free.dev/whatsapp/incoming" 
+# 👇 Fini ngrok, on met l'adresse directe du VPS 👇
+ngrok_url = "http://vpshosted.ddnsfree.com:8002/whatsapp/incoming"
 
 url = "https://waba-sandbox.360dialog.io/v1/configs/webhook"
 
